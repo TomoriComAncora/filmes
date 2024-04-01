6686e91280f4ee6467e128fb6ddb628d4b0577a5
TomoriComAncora <lucas.fernando@ufms.br>
2024-04-01 19:03:51 -0400

    pagina de favoritos

---
+++ src/pages/Favoritos/index.jsx
@@ -0,0 +1,13 @@
+import {useState, useEffect} from 'react'
+import "./Favoritos.css"
+
+function Favoritos() {
+
+  return (
+    <div>
+        <h1>Página de favoritos</h1>
+    </div>
+  )
+}
+
+export default Favoritos;
